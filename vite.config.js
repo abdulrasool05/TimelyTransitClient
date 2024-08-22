a09a3480121de260fc792580fddbf895770aaa9f
Abdul Rasool <146143358+abdulrasool05@users.noreply.github.com>
2024-08-22 00:55:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',   
-    port: process.env.FRONTEND_PORT || 3000  
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.FRONTEND_PORT || 3000  
-  }
 });
